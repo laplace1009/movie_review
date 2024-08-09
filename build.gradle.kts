6fd2fd3763c5a7a1d9a0215f4be01d8b49543aac
laplace1009 <laplace1009@gmail.com>
2024-08-09 16:06:12 +0900

    java & kotlin set up

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "1.9.24"
     kotlin("plugin.spring") version "1.9.24"
@@ -7,6 +9,7 @@ plugins {
 
 group = "tobyspring"
 version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 java {
     toolchain {
