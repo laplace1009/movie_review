c2ba12d6b7596609a8acab52a48a8378eebc32e5
laplace1009 <laplace1009@gmail.com>
2024-08-09 15:32:20 +0900

    Update .gitignore

---
+++ src/main/kotlin/com/laplace/movie_review/MovieReviewApplication.kt
@@ -0,0 +1,11 @@
+package com.laplace.movie_review
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MovieReviewApplication
+
+fun main(args: Array<String>) {
+    runApplication<MovieReviewApplication>(*args)
+}
