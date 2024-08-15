ac60f484d632d7d0e7e6f5c502a7f796dc2611bf
laplace1009 <laplace1009@gmail.com>
2024-08-15 22:05:50 +0900

    MainController test 코드 작성

--- src/main/kotlin/com/laplace/movie_review/controller/MainController.kt
+++ src/main/kotlin/com/laplace/movie_review/controller/MainController.kt
@@ -1,7 +1,6 @@
 package com.laplace.movie_review.controller
 
 import org.springframework.stereotype.Controller
-import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
@@ -9,11 +8,5 @@ class MainController {
     @GetMapping("/")
     fun index(): String {
         return "index"
-    }
-
-    @GetMapping("/home")
-    fun home(model: Model): String {
-//        model.addAttribute("name")
-        return "home"
     }
 }
---
+++ src/test/kotlin/com/laplace/movie_review/controller/MainControllerTest.kt
@@ -0,0 +1,31 @@
+package com.laplace.movie_review.controller
+
+import com.laplace.movie_review.service.AuthService
+import com.laplace.movie_review.service.TokenService
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+
+@WebMvcTest(MainController::class)
+class MainControllerTest {
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @MockBean
+    private lateinit var authService: AuthService
+
+    @MockBean
+    private lateinit var tokenService: TokenService
+
+    @Test
+    fun `GET index should return index page`() {
+        mockMvc.perform(get("/"))
+            .andExpect {
+                status().isOk
+            }
+    }
+}
