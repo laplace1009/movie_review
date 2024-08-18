4c597118ba2f1c8086701c2a2c63e240b479952b
laplace1009 <laplace1009@gmail.com>
2024-08-18 23:50:30 +0900

    LikeController Test 작성

--- src/main/kotlin/com/laplace/movie_review/controller/LikeController.kt
+++ src/main/kotlin/com/laplace/movie_review/controller/LikeController.kt
@@ -1,16 +1,24 @@
 package com.laplace.movie_review.controller
 
+import com.laplace.movie_review.dto.like.LikeCreateDTO
+import com.laplace.movie_review.service.LikeService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("/like")
-class LikeController {
+@RestController
+@RequestMapping(("/like"))
+class LikeController(private val likeService: LikeService) {
     @PostMapping
     fun addLike(
         @RequestParam accountId: Long,
         @RequestParam reviewId: Long
-    ) {
-
+    ): ResponseEntity<LikeCreateDTO> {
+        val likeCreateDTO = LikeCreateDTO(accountId, reviewId)
+        likeService.createLike(likeCreateDTO)
+        return ResponseEntity.status(HttpStatus.CREATED).body(likeCreateDTO)
     }
 }
